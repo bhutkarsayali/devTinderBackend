9d787b7c0dc05ef4dd1063a4ef8a60019b091300
Sayali <bhutkarsayali@gmail.com>
2026-09-01 15:38:32 +0530

    GET API for connections

--- src/routes/user.js
+++ src/routes/user.js
@@ -28,4 +28,33 @@ userRouter.get("/user/requests/received", userAuth, async (req, res) => {
   }
 });
 
+// Get connections
+userRouter.get("/user/connections", userAuth, async (req, res) => {
+  try {
+    const loggedInUser = req.user;
+
+    //get all the connections for logged in user
+    const connectionRequests = await ConnectionRequest.find({
+      $or: [
+        { toUserId: loggedInUser._id, status: "accepted" },
+        { fromUserId: loggedInUser._id, status: "accepted" },
+      ],
+    })
+      .populate("fromUserId", USER_SAFE_DATA)
+      .populate("toUserId", USER_SAFE_DATA);
+
+    const data = connectionRequests.map((row) => {
+      if (row.fromUserId._id.toString() === loggedInUser._id.toString()) {
+        return row.toUserId;
+      }
+      return row.fromUserId;
+    });
+    res.json({ data });
+  } catch (err) {
+    res.status(400).send({ message: err.message });
+  }
+});
+
+
+
 module.exports = userRouter;
