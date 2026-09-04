3f72b63d192a74afd4462b137020fb2f5015d56b
Sayali <bhutkarsayali@gmail.com>
2026-09-04 11:07:19 +0530

    CORS issue resolved by adding cors configs and sent user as a response after login

--- src/routes/auth.js
+++ src/routes/auth.js
@@ -61,7 +61,8 @@ authRouter.post("/login", async (req, res) => {
         expires: new Date(Date.now() + 8 * 3600000),
         httpOnly: true,
       });
-      res.send("Login Successful");
+      // res.send("Login Successful!" + user);
+      res.send(user);
     } else {
       throw new Error("Password is not correct");
     }
